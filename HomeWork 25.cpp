d968a11d01f1daad978e1bf4139ccf27191ed3df
User <User@WIN-D6I7BGDFHRA>
2022-10-17 21:05:52 +0200

    Добавьте файлы проекта.

---
+++ HomeWork 25.cpp
@@ -0,0 +1,89 @@
+﻿#include <iostream>
+
+
+int main()
+{
+    //Task 1
+    const int size = 5;
+    int i = 0;
+    int *arr = new int[size]{1, 2, 3, 4, 5};
+    int *copyarr = new int[size];
+
+
+    for (int* ptr = arr; ptr <= &arr[4]; ptr++)
+    {
+        copyarr[i++] = *ptr;
+    }
+
+
+    for (int* ptr = copyarr; ptr <= &copyarr[4]; ptr++)
+    {
+        std::cout << *ptr << " ";
+    }
+    std::cout << "\n\n";
+
+
+    delete[] arr;
+    delete[] copyarr;
+
+
+
+    //Task 2
+    const int size_arr = 5;
+    int* array = new int[size_arr] {1, 2, 3, 4, 5};
+
+
+    for (int* ptr = array + 4; ptr >= &array[0]; ptr--)
+    {
+        std::cout << *ptr << " ";
+    }
+    std::cout << "\n\n";
+
+
+    delete[] array;
+
+
+
+    //Task 3
+    int num = 0, f = 0, s = 0;
+    const int size_arraychik = 5;
+    int** arraychik = new int* [size_arraychik];
+    int** copyarraychik = new int* [size_arraychik];
+
+
+    for (int i = 0; i < size_arraychik; i++)
+    {
+        arraychik[i] = new int[size_arraychik];
+        for (int j = 0; j < size_arraychik; j++)
+        {
+            arraychik[i][j] = num++;
+        }
+        num = 0;
+    }
+
+
+    for (int i = 0; i < size_arraychik; i++)
+    {
+        copyarraychik[i] = new int[size_arraychik];
+    }
+
+
+    for (int** ptr = arraychik + 16; ptr >= &arraychik[0]; ptr--)
+    {
+        copyarraychik[f++][s] = **ptr;
+        if (f % 10 == 0)
+        {
+            s++;
+        }
+    }
+
+
+    for (int** ptr = copyarraychik; ptr <= &copyarraychik[0]; ptr++)
+    {
+        std::cout << **ptr << " ";
+    }
+
+
+    delete[] arraychik;
+    delete[] copyarraychik;
+}
